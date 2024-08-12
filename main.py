289a04c8ec2b4d26058bd820824ce39304259627
Hassan Muhammad Yousuf <alih3766@gmail.com>
2024-08-12 14:38:10 +0200

    visualize a 2D grid of nodes

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ import matplotlib.pyplot as plt
 import math
 
 # creating Nodes 
-Nodes = np.array([[0,0],[0,1],[1,0],[1,1]])
+left = 0.1
+right = 0.02
+height = 0.05
+
+nb_elements = 10
+
+# Nodes = np.array([[0,0],[0,1],[1,0],[1,1]]) (for testing purposes)
+Nodes = []
+
+for x in np.linspace(0, left , nb_elements):
+    for y in np.linspace(0, height , nb_elements):
+        Nodes.append([x,y])
+ 
+
 
 #Displaying Nodes
-plt.plot(Nodes[:,0],Nodes[:,1], 'o')
+points = np.array(Nodes)
+plt.plot(points[:,0],points[:,1], 'o')
 
 plt.show()
 
