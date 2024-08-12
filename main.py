eef82bd99444513699133c2109042decdad54475
Hassan Muhammad Yousuf <alih3766@gmail.com>
2024-08-12 14:49:13 +0200

    Adding Curve

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ left = 0.1
 right = 0.02
 height = 0.05
 
-nb_elements = 10
+nb_elements = 20
 
 # Nodes = np.array([[0,0],[0,1],[1,0],[1,1]]) (for testing purposes)
 Nodes = []
 
 for x in np.linspace(0, left , nb_elements):
-    for y in np.linspace(0, height , nb_elements):
-        Nodes.append([x,y])
+    if x < right:
+        y0 = math.sqrt(right**2 - x**2)
+        for y in np.linspace(y0, height , nb_elements):
+            Nodes.append([x,y])
+
+    else:
+        for y in np.linspace(0, height , nb_elements):
+            Nodes.append([x,y])
  
 
 
