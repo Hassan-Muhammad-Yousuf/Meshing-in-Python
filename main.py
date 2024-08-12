865d6d18926231bf3f2206d5e01c068f3c696eaa
Hassan Muhammad Yousuf <alih3766@gmail.com>
2024-08-12 14:24:15 +0200

    Basic Structure using manual Nodes

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+# creating Nodes 
+Nodes = np.array([[0,0],[0,1],[1,0],[1,1]])
+
+#Displaying Nodes
+plt.plot(Nodes[:,0],Nodes[:,1], 'o')
+
+plt.show()
+
